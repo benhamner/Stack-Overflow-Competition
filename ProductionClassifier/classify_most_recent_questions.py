dadbf6e5f30d40fdb17f94ef029017037ca3666b
Ben Hamner <ben@benhamner.com>
2013-02-04 01:11:26 -0800

    now saving to postgres db

--- ProductionClassifier/classify_most_recent_questions.py
+++ ProductionClassifier/classify_most_recent_questions.py
@@ -2,11 +2,14 @@ import data_io
 from datetime import datetime
 from features import FeatureMapper, SimpleTransform
 import pandas as pd
+import psycopg2
 import requests
 
 def get_most_recent_questions(num_questions=30):
     r = requests.get("http://api.stackexchange.com/2.1/questions?page=1&pagesize=%d&order=desc&sort=activity&site=stackoverflow" % num_questions)
     recent_questions = r.json["items"]
+
+    recent_questions = [q for q in recent_questions if "user_id" in q["owner"]]
 
     question_ids = [str(question["question_id"]) for question in recent_questions]
     user_ids = [str(question["owner"]["user_id"]) for question in recent_questions]
@@ -34,7 +37,8 @@ def get_most_recent_questions(num_questions=30):
 
         raw_question["tags"].extend(["" for x in range(5)])
 
-        question = { "PostCreationDate" : datetime.fromtimestamp(int(raw_question["creation_date"]))
+        question = { "PostId" : raw_question["question_id"]
+                   , "PostCreationDate" : datetime.fromtimestamp(int(raw_question["creation_date"]))
                    , "OwnerUserId" : raw_question["owner"]["user_id"]
                    , "OwnerCreationDate" : datetime.fromtimestamp(int(owner_creation_date))
                    , "ReputationAtPostCreation" : raw_question["owner"]["reputation"]
@@ -55,11 +59,56 @@ def get_most_recent_questions(num_questions=30):
         for k in q.keys():
             questions_for_conversion_to_df[k].append(q[k])
     data = pd.DataFrame(questions_for_conversion_to_df)
-    return data
+    return data, questions
+
+def copy_to_postgres(questions, prob_open):
+    conn = psycopg2.connect('dbname=stack user=postgres password=sx7%8rBSgB3SPuytB535')
+    cur = conn.cursor()
+
+    for question, prob in zip(questions, prob_open):
+        cur.execute("""SELECT COUNT(*)
+                       FROM questions
+                       WHERE post_id=%(PostId)s""",
+                    question)
+        res = cur.fetchone()
+        print(res)
+        if res[0]>0:
+            continue
+        question["close_likelihood"] = prob
+        cur.execute("""INSERT INTO questions (post_id,
+                                              post_creation_date,
+                                              owner_user_id,
+                                              owner_creation_date,
+                                              reputation_at_post_creation,
+                                              owner_undeleted_answer_count_at_post_time,
+                                              title,
+                                              body_html,
+                                              tag1,
+                                              tag2,
+                                              tag3,
+                                              tag4,
+                                              tag5,
+                                              close_likelihood)
+                       VALUES (%(PostId)s,
+                               %(PostCreationDate)s,
+                               %(OwnerUserId)s,
+                               %(OwnerCreationDate)s,
+                               %(ReputationAtPostCreation)s,
+                               %(OwnerUndeletedAnswerCountAtPostTime)s,
+                               %(Title)s,
+                               %(BodyMarkdown)s,
+                               %(Tag1)s,
+                               %(Tag2)s,
+                               %(Tag3)s,
+                               %(Tag4)s,
+                               %(Tag5)s,
+                               %(close_likelihood)s)
+                    """, question)
+    conn.commit()
 
 def main():
     print("Getting the newest data")
-    recent = get_most_recent_questions()
+    recent, questions = get_most_recent_questions()
 
     print("Loading the trained model")
     classifier = data_io.load_model("model.pickle")
@@ -73,6 +122,8 @@ def main():
 
     for title, prob in tuples:
         print("%s, %f" % (title, prob))
+
+    copy_to_postgres(questions, prob_open)
 
 if __name__=="__main__":
     main()
